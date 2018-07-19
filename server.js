0b1c6057ed9dc120bbf789835752d81cd107bbf5
tristanmelton <30941629+tristanmelton@users.noreply.github.com>
2018-07-19 15:56:42 -0700

    Send file right pls

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var io = require('socket.io')(http);
 
 app.get('/', function(req, res)
 {
-  res.sendFile('/index.html');
+  res.sendFile(__dirname + '/index.html');
 });
 
 http.listen(8080, function()
