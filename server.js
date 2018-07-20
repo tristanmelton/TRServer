e83117a0e5cfb763b4a365d99309905274e94bc2
tristanmelton <30941629+tristanmelton@users.noreply.github.com>
2018-07-19 17:46:02 -0700

    Added queue

--- server.js
+++ server.js
@@ -2,6 +2,13 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+	
+var tops = [];
+var jungles = [];
+var mids = [];
+var adcs = [];
+var supports = [];
+
 app.get('/', function(req, res)
 {
   res.sendFile(__dirname + '/index.html');
@@ -21,6 +28,19 @@ io.on('connection', function(socket)
 	socket.on('enterqueue', function(msg)
 	{
 		console.log(socket.id + ' joined queue with name and role ' + msg);
+		var splitted = msg.split(" ");
+		if(splitted[1] === "Tops" && tops.indexOf(splitted[0]) > -1)
+			tops.push(splitted[0]);
+		else if(splitted[1] === "Jungles" && jungles.indexOf(splitted[0]) > -1)
+			jungles.push(splitted[0]);
+		else if(splitted[1] === "Mids" && mids.indexOf(splitted[0]) > -1)
+			mids.push(splitted[0]);
+		else if(splitted[1] === "Bots" && bots.indexOf(splitted[0]) > -1)
+			bots.push(splitted[0]);
+		else if (splitted[1] === "Supports" && supports.indexOf(splitted[0] > -1)
+			supports.push(splitted[0]);
+		else
+			socket.emit("error", "You are already in queue or chose an invalid role!");
 	});
 
 });
